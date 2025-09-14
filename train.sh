a28000e3f80294fdf36a5edad43df375ea64b8d2
Boyu Zhu <2249984221@qq.com>
2025-09-14 20:53:58 +0000

    text only training

--- train.sh
+++ train.sh
@@ -2,15 +2,18 @@
 # Multimodal packing currently only supports qwen2_vl, qwen2_5_vl, qwen2_5_omni, internvl2_5/3
 # A demo for four modalities that can be run directly
 # For local datasets, it is recommended to use streaming: `--streaming true` (save memory)
-NPROC_PER_NODE=4 \
+NPROC_PER_NODE=8 \
 ENABLE_AUDIO_OUTPUT=0 \
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 VIDEO_MAX_PIXELS=50176 \
 FPS_MAX_FRAMES=12 \
 MAX_PIXELS=1003520 \
 uv run swift sft \
     --model Qwen/Qwen2.5-Omni-7B \
-    --dataset '/root/omni_guardrail/toxic_chat_label_explicit.jsonl' \
+    --dataset '/root/omni_guardrail/beaver/beaver_w_gt_sft.jsonl' \
+                '/root/omni_guardrail/toxic/toxic_w_gt_sft.jsonl' \
+                '/root/omni_guardrail/wild/wildguardmix_w_gt_sft.jsonl' \
+                '/root/omni_guardrail/aegis/Aegis_w_gt_sft.jsonl' \
     --split_dataset_ratio 0.01 \
     --train_type full \
     --attn_impl flash_attn \
@@ -26,7 +29,7 @@ uv run swift sft \
     --save_total_limit 2 \
     --logging_steps 5 \
     --max_length 4096 \
-    --output_dir output/toxic_chat_large_batch \
+    --output_dir output/w_gt \
     --warmup_ratio 0.05 \
     --dataloader_num_workers 16 \
     --dataset_num_proc 128 \
