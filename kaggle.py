4a68b4f0974a4d991c7cadc1f24289435acd7495
Boyu Zhu <2249984221@qq.com>
2025-10-07 11:21:26 -0700

    kaggle safesora

--- kaggle.py
+++ kaggle.py
@@ -8,3 +8,6 @@ print("Path to dataset files:", path)
 path = kagglehub.dataset_download("mateohervas/dcsass-dataset")
 
 print("Path to dataset files:", path)
+
+from huggingface_hub import hf_hub_download
+hf_hub_download(repo_id="PKU-Alignment/SafeSora-Label", filename="videos.tar.gz", repo_type="dataset", local_dir="/ms-swift/data/safesora")
